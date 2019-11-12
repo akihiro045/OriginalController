28f2cba641586e65480f9165279f0464afeee13f
akina <g1727045@st.t-kougei.ac.jp>
2019-11-13 01:09:08 +0900

    土台

---
+++ controller/Assets/scripts/DoSomething.cs
@@ -0,0 +1,82 @@
+﻿// Unityでシリアル通信、Arduinoと連携する雛形
+// 受信データでGameObjectを制御するソース
+// 例えば空のGameObjectでも作って、それに関連付けする
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoSomething : MonoBehaviour
+{
+    // 制御対象のオブジェクト用に宣言しておいて、Start関数内で名前で検索
+    // 制御対象のオブジェクト名がPlayerで、Playerに関連付けられたスクリプトにPlayerの処理が書かれている前提
+    GameObject targetObject;
+    playerController targetScript;
+
+    // シリアル通信のクラス、クラス名は正しく書くこと
+    public SerialHandler serialHandler;
+
+    void TryParseByte(string message, int startIndex, int length)   //試作中
+    {
+        string receivedData;
+        receivedData = message.Substring(5, 1);
+        byte sw;
+        byte.TryParse(receivedData, out sw);
+    }
+
+    void Start()
+    {
+        // 制御対象のオブジェクトを取得
+        targetObject = GameObject.Find("Player");
+        // 制御対象に関連付けられたスクリプトを取得、一般的にはこのスクリプトのメンバにアクセスして処理をすることが多いと思うので。
+        targetScript = targetObject.GetComponent<playerController>();
+
+        // 信号受信時に呼ばれる関数としてOnDataReceived関数を登録
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    void Update()
+    {
+        //文字列を送信するなら例えばココ
+        //serialHandler.Write("hogehoge");
+    }
+
+    //受信した信号(message)に対する処理
+    void OnDataReceived(string message)
+    {
+        // ここでデコード処理等を記述
+        if (message == null)
+            return;
+
+        // 受け取ったデータを数値に変換
+        if (message[0] == 'S' && message[message.Length - 1] == 'E')
+        {
+            #region 必要に応じて変更すべき箇所
+
+            string receivedData;
+
+            // 必要な文字部分のバイト数（範囲）は常に把握する
+            receivedData = message.Substring(1, 4);
+            Debug.Log(receivedData);
+
+            int vol;
+            int.TryParse(receivedData, out vol);
+
+            receivedData = message.Substring(5, 1);
+            byte sw0;
+            byte.TryParse(receivedData, out sw0);
+
+            receivedData = message.Substring(6, 1);
+            byte sw1;
+            byte.TryParse(receivedData, out sw1);
+
+            targetScript.vol = vol;
+            targetScript.sw[0] = sw0;
+            targetScript.sw[1] = sw1;
+            // 必要な文字部分を抽出したら、データ形式に合わせてデコード、例えば以下のように。
+            //float.TryParse(receivedData, out data);
+
+            #endregion
+        }
+    }
+}
---
+++ controller/Assets/scripts/playerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour
+{
+    float moveValue;
+    public float jumpValue = 5f;
+
+    public Text textInfo;
+    string str;
+
+    public int vol;
+    public byte[] sw = new byte[2];
+    public byte[] swOld = new byte[2];
+
+    void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            this.transform.position += new Vector3(0f, 0f, moveValue);
+        if (Input.GetKey(KeyCode.DownArrow))
+            this.transform.position += new Vector3(0f, 0f, -moveValue);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.position += new Vector3(-moveValue, 0f, 0f);
+        if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.position += new Vector3(moveValue, 0f, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpValue;
+        }
+
+        if (swOld[0] == 1 && sw[0] == 0)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpValue;
+        }
+        if (0 == sw[1])
+        {
+            this.transform.position += new Vector3(0f, 0f, moveValue);
+        }
+    }
+
+    void DebugText()
+    {
+        //str = string.Format("{0:F2}, {1:F2}, {2:F2}", this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        str = string.Format("{0} {1} {2}", vol, sw[0], sw[1]);
+        textInfo.text = str;
+    }
+
+    void Start()
+    {
+        moveValue = 0.1f;
+        jumpValue = 5f;
+
+        DebugText();
+
+        vol = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            sw[i] = 1;
+            swOld[i] = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+
+        DebugText();
+
+        for (int i = 0; i < sw.Length; i++)
+        {
+            sw[i] = swOld[i];
+        }
+    }
+}
